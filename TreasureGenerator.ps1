cf13aa1bb5020df3acc61942897b6d17e9d2b675
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2018-04-21 17:12:45 -0700

    Making progress

--- TreasureGenerator.ps1
+++ TreasureGenerator.ps1
@@ -1,5 +1,5 @@
 param(
-    [Parameter(Mandatory=$true)]$Type,
+    $Type,
     [switch]$ManualRollMode
 )
 function roll {
@@ -14,7 +14,26 @@ function Get-Treasure {
     param($Type, $TreasureParsed)
     switch ($Type) {
         mundane { 
-            $MundaneParsed = $TreasureParsed[1].split(":")
+            $MundaneParsed = $TreasureParsed[0].split(";")
+            function SubRoll ([int]$intArray) {
+                $roll = roll 1d100
+                $MundaneParsed[$intArray] | foreach {
+                    $each = $_.split(":")
+                    if(($each[0]..$each[1]) -match $roll){
+                        return $each[3]
+                    }
+                }
+            }
+            
+            $type = SubRoll -intArray 0
+            switch ($type) {
+                "Alchemical item" {SubRoll -intArray 1}
+                "Armor" {SubRoll -intArray 2}
+                "Shield" {SubRoll -intArray 3}
+                "Weapons" {SubRoll -intArray 4}
+                "Tools and gear" {SubRoll -intArray 5}
+            }
+            
             $OutputTreasure = [PSCustomObject]@{
                 Type = $MundaneParsed[0]
                 Treasure = $Choice[1]
@@ -32,7 +51,7 @@ $TreasureTypes = @("magic","mundane","gems","art")
 foreach ($eachType in $TreasureTypes){
     $d100roll = roll 1d100
     if ($d100roll -gt (100 - ($level * 5))){
-        Get-Treasure -Type $eachType -TreasureParsed $TreasureParsed
+        Get-Treasure -Type "mundane" -TreasureParsed $TreasureParsed
     }
 }
 $CoinsMax = (roll "1d$([int]($level / 2))") * ([math]::floor(($level / 5)) + 1) * 100
