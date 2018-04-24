0e71ec08169264f1008593482ea9dd2964b7c1c1
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2018-04-23 17:32:30 -0700

    Making progress, reorganizing the TreasureData

--- TreasureGenerator.ps1
+++ TreasureGenerator.ps1
@@ -1,10 +1,12 @@
 class Treasure {
     $Level
-    $Coins
-    [Bool]$ManualRollMode = $false
+    $Gold
     $TreasureParsed
+    [System.Collections.ArrayList]$Items
+    [Bool]$ManualRollMode = $false
     
     Treasure ($Level) {
+        $this.Items = New-Object System.Collections.ArrayList
         $this.Level = $Level
         $this.TreasureParsed = ((Get-Content ./TreasureData.txt) -join("")) -split('-----')
         $TreasureTypes = @("magic","mundane","gems","art")
@@ -35,16 +37,45 @@ class Treasure {
         }
     }
 
-    [void] GetMundane () {}
+    [void] GetParse ([int]$ArrayInt,[int]$TreasureInt) {
+        $roll = $this.roll("1d100")
+        $SectionParsed = $this.TreasureParsed[$TreasureInt].split(";")
+        $SectionParsed[$ArrayInt].split('*') | foreach {
+            $each = $_.split(":")
+            if(($each[0]..$each[1]) -match $roll){
+                if ($ArrayInt -eq 0 -AND $TreasureInt -eq 0){
+                    return $each[2]
+                }
+                else {
+                    $this.items.Add($each[2])
+                }
+            }
+        }
+    }
+
+    [void] GetMundane () {     
+        $type = $this.GetParse(0,0)
+        switch ($type) {
+            "Alchemical item" {$this.GetParse(1,0)}
+            "Armor" {$this.GetParse(2,0)}
+            "Shield" {$this.GetParse(3,0)}
+            "Weapons" {$this.GetParse(4,0)}
+            "Tools and gear" {$this.GetParse(5,0)}
+        }
+    }
     [void] GetMagic () {}
     [void] GetGem () {}
     [void] GetArt () {}
+    
+    [void] GetArmor () {}
+    [void] GetWeapon () {}
+    [void] GetShield () {}
 
     [void] GetGold () {
         $CoinSides = "1d$([int]($This.level / 2))"
         $CoinsMax = $this.roll($CoinSides) * ([math]::floor(($this.level / 5)) + 1) * 100
         $CoinsMin = $CoinsMax * .75
-        $this.Coins = [int](Get-Random -min $CoinsMin -max $CoinsMax)
+        $this.Gold = [int](Get-Random -min $CoinsMin -max $CoinsMax)
     }
 }
 
