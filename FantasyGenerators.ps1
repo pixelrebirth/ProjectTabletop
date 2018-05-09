f3e249760915697c3a7a54971dc0e0aea7b8b56a
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2018-05-08 21:09:34 -0700

    Final for now generator, starting on main objective

--- FantasyGenerators.ps1
+++ FantasyGenerators.ps1
@@ -1,12 +1,23 @@
-$SearchName = 'dungeon-descriptions'
-$ie = New-Object -ComObject "InternetExplorer.Application"
-# $ie.visible = $true
-$ie.silent = $true
-$ie.Navigate("https://www.fantasynamegenerators.com/$SearchName.php")
-while($ie.ReadyState -ne 4) {start-sleep -m 100}
-while ($ie.Busy) {Start-Sleep -Milliseconds 50}
-$body = $ie.document.getElementById('result').innerText
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie)
-Remove-Variable ie
+[cmdletbinding()]
+param()
+DynamicParam {
+    [Scriptblock]$List = {Get-Content $PSScriptRoot\Data\FantasyDescPhp.txt}
+    return ./Functions/Get-DynamicParam.ps1 -ParamName "SearchName" -ParamCode $List
+}
 
-$body
+begin {
+    $SearchName = $PsBoundParameters['SearchName']
+}
+process {
+    $ie = New-Object -ComObject "InternetExplorer.Application"
+    $ie.silent = $true
+    $ie.Navigate("https://www.fantasynamegenerators.com/$SearchName.php")
+    while($ie.ReadyState -ne 4) {start-sleep -m 100}
+    while ($ie.Busy) {Start-Sleep -Milliseconds 50}
+    $body = $ie.document.getElementById('result').innerText
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie)
+    Remove-Variable ie
+}
+end {
+ return $body
+}
--- TrapGenerator.ps1
+++
@@ -1,1 +0,0 @@
-return (Invoke-WebRequest -uri 'https://donjon.bin.sh/m20/random/rpc.cgi?type=Level+1+Room+Trap&n=10').content  -split('","') -replace('\[|\]|"','') 
