75de4012d66bf34f167ff71f909a04f074b336cf
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2018-06-02 09:30:37 -0700

    Activated Sheet

--- Functions/Export-DungeonMasterSheet.ps1
+++ Functions/Export-DungeonMasterSheet.ps1
@@ -15,213 +15,213 @@ function Export-DungeonMasterSheet {
     #     $BlobData.NPCKits[0].npcgroups | select Level,Name,Race,Class,Vitals,Attack | ft
     #     $BlobData.NPCKits[0].npcgroups | select Name,Appearance,Traits | fl
     #     } 
-    # "<div class=`'wide`'>"
-    # '### Level 3 Treasure Rolls'
-    # "Roll | Items | Gold"
-    # "-----:|-----:|----:"
+    "<div class=`'wide`'>"
+    '### Level 3 Treasure Rolls'
+    "Roll | Items | Gold"
+    "-----:|-----:|----:"
 
-    # foreach ($treasure in $BlobData.Treasures){
-    #     if ($Count -gt 40){
-    #         "</div>"
-    #         "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | TREASURE</div>"
-    #         "\page"
-    #         "<div class=`'wide`'>"
-    #         "Roll | Items | Gold"
-    #         "-----:|-----:|----:"
-    #         $Count = 0
-    #     }
-    #     "$($treasure.dicenumber) | $(($treasure.items) -join(", ")) | $($treasure.gold)"
-    #     $Count++
-    # }
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | TREASURE</div>"
-    # "\page"
+    foreach ($treasure in $BlobData.Treasures){
+        if ($Count -gt 40){
+            "</div>"
+            "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | TREASURE</div>"
+            "\page"
+            "<div class=`'wide`'>"
+            "Roll | Items | Gold"
+            "-----:|-----:|----:"
+            $Count = 0
+        }
+        "$($treasure.dicenumber) | $(($treasure.items) -join(", ")) | $($treasure.gold)"
+        $Count++
+    }
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | TREASURE</div>"
+    "\page"
 
-    # "# Names"
-    # Foreach ($NameType in $BlobData.NameVariants){
-    #     "### $($NameType.NameType.ToUpper())"
-    #     "$($NameType.names[0..9].trim() -join(", "))"
-    # }
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | NAMES</div>"
-    # "\page"
+    "# Names"
+    Foreach ($NameType in $BlobData.NameVariants){
+        "### $($NameType.NameType.ToUpper())"
+        "$($NameType.names[0..9].trim() -join(", "))"
+    }
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | NAMES</div>"
+    "\page"
     
-    # "<div class=`'wide`'>"
-    # "# Panic Locations"
-    # Foreach ($Location in $BlobData.PanicLocations){
-    #     "### $($Location.name)"
+    "<div class=`'wide`'>"
+    "# Panic Locations"
+    Foreach ($Location in $BlobData.PanicLocations){
+        "### $($Location.name)"
         
-    #     Get-Content -Path ./data/LocationUri.txt | Where {$_ -match "^$($Location.name),(.*)$"} | Out-Null
-    #     $uri = $Matches[1]
+        Get-Content -Path ./data/LocationUri.txt | Where {$_ -match "^$($Location.name),(.*)$"} | Out-Null
+        $uri = $Matches[1]
 
-    #     $GetLore = invoke-webrequest -uri "http://eberron.wikia.com$uri"
-    #     $Lore = $((($GetLore.ParsedHtml.body.outertext) -split('\n')) | where {$_.length -gt 100})
+        $GetLore = invoke-webrequest -uri "http://eberron.wikia.com$uri"
+        $Lore = $((($GetLore.ParsedHtml.body.outertext) -split('\n')) | where {$_.length -gt 100})
 
-    #     foreach ($line in $lore){
-    #         $ReducedLine = $($line -replace("^[A-Za-z ]+ Edit",""))
-    #         ""
-    #         "$ReducedLine"
-    #     }
-    # }
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | LOCATIONS</div>"
-    # "\page"
+        foreach ($line in $lore){
+            $ReducedLine = $($line -replace("^[A-Za-z ]+ Edit",""))
+            ""
+            "$ReducedLine"
+        }
+    }
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | LOCATIONS</div>"
+    "\page"
 
-    # $count = 1
-    # "<div class=`'wide`'>"
-    # "# Vendors"
-    # foreach ($shop in $($BlobData.Vendors)){
-    #     "### $($shop.ShopName) [$($shop.ShopType)]"
-    #     ""
-    #     "###### __ShopKeep:__"
-    #     "$($shop.ShopKeep)"
-    #     ""
-    #     "---"
-    #     "###### __Location:__"
-    #     "$($shop.Location)"
-    #     ""
-    #     "---"
-    #     "###### __Description:__"
-    #     "$($shop.Description)"
-    #     ""
-    #     "|Item|Description|"
-    #     "|:-----|:-----|"
-    #     foreach ($item in $shop.items){
-    #         "|$($item.name)|$($item.notes)|"
-    #     }
+    $count = 1
+    "<div class=`'wide`'>"
+    "# Vendors"
+    foreach ($shop in $($BlobData.Vendors)){
+        "### $($shop.ShopName) [$($shop.ShopType)]"
+        ""
+        "###### __ShopKeep:__"
+        "$($shop.ShopKeep)"
+        ""
+        "---"
+        "###### __Location:__"
+        "$($shop.Location)"
+        ""
+        "---"
+        "###### __Description:__"
+        "$($shop.Description)"
+        ""
+        "|Item|Description|"
+        "|:-----|:-----|"
+        foreach ($item in $shop.items){
+            "|$($item.name)|$($item.notes)|"
+        }
         
-    #     if ($count -eq 3){
-    #         "</div>"
-    #         "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | VENDORS</div>"
-    #         "\page"
-    #         "<div class=`'wide`'>"
-    #     }
-    #     $count++
-    # }
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | VENDORS</div>"
-    # "\page"
+        if ($count -eq 3){
+            "</div>"
+            "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | VENDORS</div>"
+            "\page"
+            "<div class=`'wide`'>"
+        }
+        $count++
+    }
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | VENDORS</div>"
+    "\page"
     
-    # $Total = 0
-    # foreach ($Desc in $BlobData.Descriptions){                    
-    #     if ($Desc.values -ne $null){
-    #         $value = $(($desc.values) -replace("^0 |.*[\=\-][\=\-]+.*",''))
-    #         $total += $value.length
+    $Total = 0
+    foreach ($Desc in $BlobData.Descriptions){                    
+        if ($Desc.values -ne $null){
+            $value = $(($desc.values) -replace("^0 |.*[\=\-][\=\-]+.*",''))
+            $total += $value.length
             
-    #         ""
-    #         if ($total -gt 4500){
-    #             "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DESCRIPTIONS</div>"
-    #             "\page"
-    #             $total = 0
-    #         }
+            ""
+            if ($total -gt 4500){
+                "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DESCRIPTIONS</div>"
+                "\page"
+                $total = 0
+            }
             
-    #         "### $($desc.keys)"
-    #         "$value"
-    #     }
-    # }
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DESCRIPTIONS</div>"
-    # "\page"
+            "### $($desc.keys)"
+            "$value"
+        }
+    }
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DESCRIPTIONS</div>"
+    "\page"
     
-    # "<div class=`'wide`'>"
-    # "### Weather"
-    # "|Weather|Effect|"
-    # "|:-----|:-----|"
-    # foreach ($eachWeather in $($BlobData.Weather)){
-    #     "|$($eachWeather.weather)|$($eachWeather.effect)|"
-    # }
+    "<div class=`'wide`'>"
+    "### Weather"
+    "|Weather|Effect|"
+    "|:-----|:-----|"
+    foreach ($eachWeather in $($BlobData.Weather)){
+        "|$($eachWeather.weather)|$($eachWeather.effect)|"
+    }
 
-    # "### Prophecies"
-    # "||"
-    # "|:-----|"
-    # foreach ($prophecy in $($BlobData.Prophecies)){
-    #     "|$prophecy|"
-    # }
+    "### Prophecies"
+    "||"
+    "|:-----|"
+    foreach ($prophecy in $($BlobData.Prophecies)){
+        "|$prophecy|"
+    }
 
-    # "### Omens"
-    # "||"
-    # "|:-----|"
-    # foreach ($omen in $($BlobData.Omens)){
-    #     "|$omen|"
-    # }
+    "### Omens"
+    "||"
+    "|:-----|"
+    foreach ($omen in $($BlobData.Omens)){
+        "|$omen|"
+    }
 
-    # "### Graffiti"
-    # "||"
-    # "|:-----|"
-    # foreach ($graf in $($BlobData.Graffiti)){
-    #     "|$graf|"
-    # }
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DONJON</div>"
-    # "\page"
+    "### Graffiti"
+    "||"
+    "|:-----|"
+    foreach ($graf in $($BlobData.Graffiti)){
+        "|$graf|"
+    }
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DONJON</div>"
+    "\page"
 
-    # "<div class=`'wide`'>"
-    # "### Castles"
-    # "||"
-    # "|:-----|"
-    # foreach ($Castle in $($BlobData.Castles)){
-    #     "|$Castle|"
-    # }
+    "<div class=`'wide`'>"
+    "### Castles"
+    "||"
+    "|:-----|"
+    foreach ($Castle in $($BlobData.Castles)){
+        "|$Castle|"
+    }
 
-    # "### Legendary Items"
-    # "||"
-    # "|:-----|"
-    # foreach ($Legend in $($BlobData.LegendaryItems)){
-    #     "|$Legend|"
-    # }
+    "### Legendary Items"
+    "||"
+    "|:-----|"
+    foreach ($Legend in $($BlobData.LegendaryItems)){
+        "|$Legend|"
+    }
     
-    # "### Tomes"
-    # "||"
-    # "|:-----|"
-    # foreach ($Tome in $($BlobData.Tomes)){
-    #     "|$Tome|"
-    # }    
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DONJON </div>"
-    # "\page"
+    "### Tomes"
+    "||"
+    "|:-----|"
+    foreach ($Tome in $($BlobData.Tomes)){
+        "|$Tome|"
+    }    
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DONJON </div>"
+    "\page"
 
-    # $Count = 1
-    # "### Pockets"
-    # "|Dice Roll|Pockets|"
-    # "|:-----:|:-----|"
-    # foreach ($Pocket in $($BlobData.Pockets)){
-    #     "|$Count|$Pocket|"
-    #     $Count++
-    # }
+    $Count = 1
+    "### Pockets"
+    "|Dice Roll|Pockets|"
+    "|:-----:|:-----|"
+    foreach ($Pocket in $($BlobData.Pockets)){
+        "|$Count|$Pocket|"
+        $Count++
+    }
     
-    # $Count = 1
-    # "<div class=`'wide`'>"
-    # "### GiantBags"
-    # "|Dice Roll|GiantBags|"
-    # "|:-----:|:-----|"
-    # foreach ($GiantBag in $($BlobData.GiantBags)){
-    #     "|$Count|$GiantBag|"
-    #     $Count++
-    # }
+    $Count = 1
+    "<div class=`'wide`'>"
+    "### GiantBags"
+    "|Dice Roll|GiantBags|"
+    "|:-----:|:-----|"
+    foreach ($GiantBag in $($BlobData.GiantBags)){
+        "|$Count|$GiantBag|"
+        $Count++
+    }
 
     
-    # $Count = 1
-    # "### Traps"
-    # "|Dice Roll|Trap|DCs|"
-    # "|:-----:|:-----|"
-    # foreach ($Trap in $($BlobData.Traps | select -unique)){
-    #     $Trap = ($Trap -split('\;\\n    ')) 
-    #     "|$Count|$($Trap[0])|$($Trap[1])|"
-    #     $Count++
-    # }
-    # "</div>"
-    # "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DICE ROLLS</div>"
-    # "\page"
+    $Count = 1
+    "### Traps"
+    "|Dice Roll|Trap|DCs|"
+    "|:-----:|:-----|"
+    foreach ($Trap in $($BlobData.Traps | select -unique)){
+        $Trap = ($Trap -split('\;\\n    ')) 
+        "|$Count|$($Trap[0])|$($Trap[1])|"
+        $Count++
+    }
+    "</div>"
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | DICE ROLLS</div>"
+    "\page"
     
-    # "# Panic-Plots"
+    "# Panic-Plots"
     
-    # foreach ($PanicPlot in $($BlobData.Inns)){
-    #     "### $($PanicPlot.Title)"
-    #     ""
-    #     "##### Summary:"
-    #     "$($PanicPlot.Summary)"
-    #     ""
-    #     "##### Twist:"
-    #     "$($PanicPlot.Twist)"
-    # }
+    foreach ($PanicPlot in $($BlobData.Inns)){
+        "### $($PanicPlot.Title)"
+        ""
+        "##### Summary:"
+        "$($PanicPlot.Summary)"
+        ""
+        "##### Twist:"
+        "$($PanicPlot.Twist)"
+    }
     "<div class=`'wide`'>"
     "# Inn"
     $Inn = $BlobData.Inn
@@ -257,6 +257,14 @@ function Export-DungeonMasterSheet {
     }
     ""
     "</div>"
-    #     $BlobData.ExtraMonsterKits.extramonsters
-    # $BlobData.Inns
+    "<div class='pageNumber auto'></div><div class='pageNumber'>1</div> <div class='footnote'>LEVEL DATA | INN</div>"
+    "\page"
+    $Count = 1
+    "### Extra Monsters"
+    "|Dice Roll|Monster|"
+    "|:-----|:-----|"
+    foreach ($monster in $BlobData.ExtraMonsterKits.extramonsters){
+        "|$Count|$monster|"
+        $Count++
+    }
 }
