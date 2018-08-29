f050deb073222716fe049049c1cdc8cf8399d77b
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2018-08-29 08:28:52 -0700

    Basic NPC Generator is complete

---
+++ PSClasses/NpcCharacter.ps1
@@ -0,0 +1,73 @@
+class Character {
+    hidden [int] $BaseDefense = 10
+    hidden [int] $RndModifier = 2
+    hidden [int] $PointsPerDifficulty = 5
+    hidden [int] $BasePoints = 10
+
+    [int] $Movement = 3
+    [int] $Health = $this.RndModifier*(20-$this.BaseDefense)
+}
+
+class Npc : Character {
+    hidden $EquipmentModifier
+    hidden [int] $Points
+    [int] $Difficulty
+    [int] $Equipment
+
+    [int] $Melee
+    [int] $Ranged
+    [int] $Magic
+    [int] $Will
+    [int] $Defense
+    
+    Npc ($Difficulty) {
+        $this.Difficulty = $Difficulty
+        $this.EquipmentModifier = $this.Difficulty
+        $this.Points = ($this.Difficulty*$this.PointsPerDifficulty)+$this.BasePoints
+        
+        $PropertyArray = ("Melee","Ranged","Magic","Will","Defense","Movement","Health")
+        $AllValues = @()
+        while ($AllValues.count -gt 7 -OR $AllValues.count -lt 3){
+            $AllValues = $this.Get_PointDistributionArray()
+        }
+        #TODO Randomize property array and assign a number from the $AllValues array
+        foreach ($Value in $AllValues){
+            $Random = Get-Random -min 0 $PropertyArray.count
+            $Property = $PropertyArray[$Random]
+            $this."$Property" += $Value
+            if ($Value - $Difficulty -gt $this.Equipment){
+                $this.Equipment = $Value - $Difficulty
+            }
+        }
+    }
+    [array] Get_PointDistributionArray () {
+        $AllValues = @()
+        $TotalValue = $this.Points
+        $CalculatedValue = 0
+        $RemainingValue = 0
+        
+        $count = 0
+        while ($TotalValue -gt 0 -or $AllValues.count -lt 7){
+            $count++
+            [int] $CalculatedValue = Get-Random -min 1 -max $this.Points
+            if ($CalculatedValue -le $this.Difficulty+$this.EquipmentModifier){
+                $RemainingValue = $TotalValue - $CalculatedValue
+                if ($RemainingValue -ge 0){
+                    $TotalValue = $RemainingValue
+                    $AllValues += $CalculatedValue
+                }
+            }
+            if ($Count -gt 100){
+                break
+            }
+        }
+        return $AllValues
+    }
+}
+1..7 | % {
+    $Difficulty = $_
+    1..10 | % {
+        $output = New-Object Npc($Difficulty)
+        $output
+    }
+} | ft *
---
+++ PSClasses/PlayerCharacter.ps1
@@ -0,0 +1,36 @@
+class PlayerCharacter {
+    [string] $CharacterName
+    [string] $PlayerName
+    
+    [int] $Level
+
+    [int] $XP
+    [int] $Heroism
+    
+    [int] $Armor_Training
+    [int] $Martial_Training
+    [int] $Shield_Training
+    
+    [int] $Health
+    [int] $Will
+    [int] $Defense
+    [int] $Movement
+    
+    [int] $Unarmed
+    [int] $One_Handed
+    [int] $Duel_Wield
+    [int] $Two_Handed
+    [int] $Reach
+
+    [int] $Short_Range
+    [int] $Medium_Range
+    [int] $Long_Range
+
+    [int] $Elemental_Magic
+    [int] $Light_Magic
+    [int] $Dark_Magic
+
+    Player () {
+
+    }
+}
